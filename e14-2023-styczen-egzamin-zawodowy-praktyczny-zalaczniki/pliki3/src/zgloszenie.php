648b6f151a0b5f1297f4d0a951fd9a451ff7c7aa
Tomasz Pieniążek <113185172+TomekPieniazek@users.noreply.github.com>
2024-11-05 21:14:57 +0100

    321

---
+++ e14-2023-styczen-egzamin-zawodowy-praktyczny-zalaczniki/pliki3/src/zgloszenie.php
@@ -0,0 +1,18 @@
+<?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {    
+        $conn = new mysqli('localhost', 'root', '', 'baza');
+        
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        
+
+        $lowisko = htmlspecialchars($_POST["lowisko"]);
+        $data = htmlspecialchars($_POST["data"]);
+        $sedzia = htmlspecialchars($_POST["sedzia"]);
+
+        $conn->query("INSERT INTO zawody_wedkarskie (Karty_wedkarskie_id, Lowisko_id, data_zawodow, sedzia) VALUES (0, $lowisko, $data, $sedzia)");
+        $conn->close();
+    }
+
+?>
